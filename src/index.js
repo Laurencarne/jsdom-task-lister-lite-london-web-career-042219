66ca6ee8e1d60936eea4a71fa29a2e09b44d83bb
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-03 15:54:14 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+});
+
+let createTaskButton = document.querySelectorAll("input")[1];
+
+createTaskButton.addEventListener("click", function (event) {
+  event.preventDefault()
+
+  let li = document.createElement("li")
+
+  let ul = document.querySelector("#tasks")
+
+  li.innerHTML = `${document.querySelector("#new-task-description").value} <button id="deleteButton">X</button>`
+
+  ul.appendChild(li)
+
+  let deleteTaskButton = li.querySelector("#deleteButton");
+
+  deleteTaskButton.addEventListener("click", function (event) {
+    event.preventDefault()
+    li.remove()
+  });
 });
